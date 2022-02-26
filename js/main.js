9646761abd1cac74e31ff9965269d998cdd090bb
Braian <braianfelipecsk123@hotmail.com>
2022-02-26 11:37:20 -0300

    finalização da sessão de calendários

--- js/main.js
+++ js/main.js
@@ -28,3 +28,48 @@ const swiper = new Swiper('.swiper', {
     },
   
   });
+
+  const swiper2 = new Swiper('.mySwiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 2,
+    spaceBetween: 20,
+    centeredSlides: false,
+    slideToClickedSlide:true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  
+  });
+
+//funções responsaveis por mudar o background do topo
+
+
+
+
+
+
+//funções responsaveis por mudar o background do calendário
